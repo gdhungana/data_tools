1a6767890bb687a66dedbaa529dd896c23905732
gdhungana <gdhungana100@gmail.com>
2020-01-24 10:52:25 -0600

    add a function to check for fields in tables of hive database

--- py/data_tools/pysp_util.py
+++ py/data_tools/pysp_util.py
@@ -16,4 +16,27 @@ def get_cols(tablename):
     cols=[str(row.col_name) for row in colrows]
     return cols
 
+def check_field(database,fieldlike,tables=None,outtables=False):
+    #sqlContext.sql("use "+database)
+    foundin=[]
+    if tables is None:
+        alltables=get_tables(database)
+        #- check tmp tables and remove from the list
+        tables= [s for s in alltables if "tmp_" !=s[:4]]
+    else:
+        #- load the database here
+        sqlContext.sql("use "+database)
+    for table in tables:
+        columns=get_cols(table)
+        matching = [s for s in columns if fieldlike in s]
+        if len(matching)>0:
+            foundin.append(table)
+            print("Matching columns found in Table: "+table+":  ",matching)
+        #else:
+        #    print("No matchin column found in Table: "+table)
+    print("Finish checking "+database+" for "+fieldlike)
+    if outtables:
+        return foundin
+    return
+
 
