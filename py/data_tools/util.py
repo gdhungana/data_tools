fdaba12f34a324834d806da820200f366d3d6777
Dhungana, Govinda (gd397w) <gd397w@us.att.com>
2020-02-24 15:39:41 -0600

    flattening of a list of strings

--- py/data_tools/util.py
+++ py/data_tools/util.py
@@ -12,4 +12,40 @@ def run_session(appname='test'):
                 .getOrCreate())
     return sparkSession
 
+def get_strings(obj):
+
+    if isinstance(obj,bool):
+        return str(obj)
+    #- unicode instance for python 2
+    if isinstance(obj,unicode):
+        if python_ver=='2':
+            return str(obj)
+    if isinstance(obj,dict):
+        #if python_ver=='2':
+        return dict([(get_strings(key),get_strings(val)) for key,val in obj.items()])
+        #elif python_ver=='3': #- python 3
+        #    return {get_strings(key):get_strings(value) for key,value in obj.items()}
+    return obj
+
+def json_loads(listofstrings):
+    """
+    listofstrings: list of strings, e.g datafr.select("properties").collect() for ecocl data
+    """
+    flat_val=[]
+    for i, val in enumerate(listofstrings):
+        if python_ver == '3':
+            try: 
+                flat_val.append(get_strings(json.loads((val[0]))))
+            except json.JSONDecodeError as je:
+                flat_val.append({})
+        else: #- for python 2 
+            try:
+                flat_val.append(get_strings(json.loads((val[0]))))
+            except: #- this should be valueError
+                flat_val.append({})
+                
+    return flat_val
+
+
+
 
