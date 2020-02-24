d493443b6092426a36b0eea7f75045eb29cdb5d6
gdhungana <gdhungana100@gmail.com>
2020-02-24 15:35:42 -0600

    Some parsing logic for pandas

--- py/data_tools/pysp_util.py
+++ py/data_tools/pysp_util.py
@@ -39,4 +39,37 @@ def check_field(database,fieldlike,tables=None,outtables=False):
         return foundin
     return
 
+def parsePandas(csvfile):
+    #- to parse a csv/tsv wtiten using hive e.g:
+    
+    df=pd.read_csv(csvfile,delimiter="\t",names=['load_ts','id','ts','properties','data_dt'],header=0)
+
+    propVals=df.properties.values
+    ndf=df.drop("properties",axis=1)
+    propParsed=[]
+    for i, val in enumerate(propVals):
+        try:
+            propParsed.append(util.get_strings(json.loads((val.lower()))))
+        except json.JSONDecodeError as je:
+            propParsed.append({})
+   
+    propDF=pd.io.json.json_normalize(propParsed)
+    parsedDF=pd.concat([ndf,propDF],axis=1)
+    return parsedDF
+
+def sortDFby(datafr,key):
+    """
+    datafr: Pandas dataframe
+    """
+    from datetime import datetime
+    if key == "load_ts":
+        load_ts=[datetime.strptime(ind,"%Y/%m/%d %H:%M:%S") for ind in datafr[key]]
+        tseries=pd.to_datetime(load_ts)
+
+    if key == "ts":
+        tseries=pd.DatetimeIndex(pd.to_datetime(datafr[key],unit='ms'))
+
+    ts,kk=tseries.sortlevel()
+    sortedDF=datafr.iloc[kk]
+    return sortedDF
 
